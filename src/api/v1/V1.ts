c20d41235eef5fdbca87caf1989b13576f6c5076
Rongyi Chen <kenchenrong@gmail.com>
2022-02-10 23:16:24 -0800

    add secret test

--- src/api/v1/V1.ts
+++ src/api/v1/V1.ts
@@ -4,7 +4,11 @@ import libraryRouter from './LibraryRouter.js'
 export default (authentication) => {
     const apiv1 = Router();
 
+    apiv1.post('/', authentication, (req, res) => {
+        res.status(200).send({ result: 'success' });
+    });
+
     apiv1.use('/library', authentication, libraryRouter);
-    
+
     return apiv1;
 };
