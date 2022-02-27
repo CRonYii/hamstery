d6a02bf1a3dbc4b9feaab4b456fbcd5413440e93
Rongyi Chen <kenchenrong@gmail.com>
2022-02-26 19:46:46 -0800

    Add static serve

--- src/Server.ts
+++ src/Server.ts
@@ -11,7 +11,7 @@ async function startServer() {
     const app = express();
 
     app.use(express.json());
-
+    app.use(express.static('public'));
     app.use("/api", api(authenticationChecker(env.SECRET)));
 
     app.listen(env.PORT, () => {
